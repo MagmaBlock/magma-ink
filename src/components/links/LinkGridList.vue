3664b0b4103c29b5f0ce3e311189f569f756a1eb
MagmaBlock <61128045+MagmaBlock@users.noreply.github.com>
2026-01-25 15:33:54 +0800

    更新友联

--- src/components/links/LinkGridList.vue
+++ src/components/links/LinkGridList.vue
@@ -214,7 +214,7 @@ const links: {
     title: "imxizhen",
     img: "https://q1.qlogo.cn/g?b=qq&nk=89525295&s=640",
     desc: "永远相信美好的事情即将发生.",
-    link: "https://www.imxizhen.asia",
+    link: "https://www.imxizhen.cn/",
   },
   {
     title: "划水",
